ed64c9f82a0d68b646447ccc1e32127a98887375
john32313 <francois.jonathan13@gmail.com>
2020-12-25 15:49:50 +0100

    made five inputs in signup

--- front/homere_front/src/component/Signup.jsx
+++ front/homere_front/src/component/Signup.jsx
@@ -1,21 +1,68 @@
 import { useState } from "react";
 
 export default function Signup() {
-  const [email, setEmail] = useState("");
+  const [info, setInfo] = useState({
+    email: "",
+    password: "",
+    passwordbis: "",
+    name: "",
+    lastname: "",
+  });
 
-  const updateEmailField = (e) => {
-    setEmail(e.target.value);
+  const updateStateInfo = (e) => {
+    setInfo((prev) => ({ ...info, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("A name was submitted: ", info);
   };
 
   return (
     <>
-      <h1>{email}</h1>
-      <input
-        type="email"
-        name="email"
-        value={email}
-        onChange={updateEmailField}
-      />
+      <h1>{JSON.stringify(info, 1, 1)}</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email : </label>
+        <input
+          type="email"
+          name="email"
+          autoComplete="off"
+          value={info.email}
+          onChange={updateStateInfo}
+        />
+        <label htmlFor="password">Password : </label>
+        <input
+          type="password"
+          name="password"
+          autoComplete="new-password"
+          value={info.password}
+          onChange={updateStateInfo}
+        />
+        <label htmlFor="passwordbis">Confirm Password: </label>
+        <input
+          type="password"
+          name="passwordbis"
+          autoComplete="off"
+          value={info.passwordbis}
+          onChange={updateStateInfo}
+        />
+        <label htmlFor="name">Name : </label>
+        <input
+          type="text"
+          name="name"
+          value={info.name}
+          onChange={updateStateInfo}
+        />
+        <label htmlFor="lastname">Lastname : </label>
+        <input
+          type="text"
+          name="lastname"
+          value={info.lastname}
+          onChange={updateStateInfo}
+        />
+
+        <input type="submit" value="Soumettre" />
+      </form>
     </>
   );
 }
